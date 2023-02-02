38908a0f205d43b7eee303cde599122e489a755c
Frankevych <andriy.frankevych@docebo.com>
2023-02-02 16:06:15 +0100

    add api gateway construct

--- lambda/hello.ts
+++ lambda/hello.ts
@@ -3,6 +3,6 @@ import { APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => ({
     statusCode: 200,
     body: JSON.stringify({
-        message: `Heila, World! You've hit ${event.path}`
+        message: `Hello, World! You've hit ${event.path}`
     }),
 });
--- lib/cdk-workshop-2-stack.ts
+++ lib/cdk-workshop-2-stack.ts
@@ -1,4 +1,5 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
 import * as lambda from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs';
 
@@ -6,10 +7,14 @@ export class CdkWorkshop2Stack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-      const hello = new lambda.Function(this, 'CDKWorkshopHelloHandler', {
+      const helloHandler = new lambda.Function(this, 'CDKWorkshopHelloHandler', {
         runtime: lambda.Runtime.NODEJS_18_X,
         code: lambda.Code.fromAsset('dist/lambda'),
         handler: 'hello.handler',
+      });
+
+      new apigw.LambdaRestApi(this, 'Endpoit', {
+        handler: helloHandler
       });
   }
 }
